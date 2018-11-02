8ccbc43268dbf2a2d6501a65f837119ace9ce1be
Marcin Wolf <wolflint@users.noreply.github.com>
2018-11-02 18:44:03 +0000

    Accordion links

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// SIDE NAVIGATION
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
@@ -11,3 +12,21 @@ function closeNav() {
     document.getElementById("main-content").style.marginRight = "0";
     document.body.style.backgroundColor = "white";
 }
+// END OF SIDE NAVIGATION
+
+// ACCORDION
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+// END OF ACCORDION
