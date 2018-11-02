e280d33f6db7c1ce018ec327f00543d1173c6589
Marcin Wolf <wolflint@users.noreply.github.com>
2018-11-02 15:47:33 +0000

    Sorted CSS

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main-content").style.marginRight = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main-content").style.marginRight = "0";
+    document.body.style.backgroundColor = "white";
+}
