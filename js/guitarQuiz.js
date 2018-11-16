f20deab86ab1324f0489166ee6013505d05812d8
Marcin Wolf <wolflint@users.noreply.github.com>
2018-11-16 16:30:56 +0000

    memory game

---
+++ js/guitarQuiz.js
@@ -0,0 +1,7 @@
+function pick(){
+  var Q1 = document.picker.Q1.value;
+  var Q2 = document.picker.Q2.value;
+  var Q3 = document.picker.Q3.value;
+  var Q4 = document.picker.Q4.value;
+
+}
--- js/guitarpicker.js
+++ js/guitarpicker.js
@@ -1,90 +1,90 @@
 "use strict";
 
-window.onload = function () {
+window.onload = function() {
 
     var quiz = {
         title: 'What superhero are you?',
 
         questions: [{
-            text: "How would you describe your personality?",
-            responses: [{
-                text: 'Im serious and dark',
-                value: 'Batman'
+                text: "How would you describe your personality?",
+                responses: [{
+                        text: 'Im serious and dark',
+                        value: 'Batman'
+                    },
+                    {
+                        text: 'Arrogant, but charming',
+                        value: 'Superman'
+                    },
+                    {
+                        text: 'Fun and easy going',
+                        value: 'The Flash'
+                    }
+                ]
             },
             {
-                text: 'Arrogant, but charming',
-                value: 'Superman'
+                text: "Why did you want to become a superhero?",
+                responses: [{
+                        text: 'For the thrills',
+                        value: 'The Flash'
+                    },
+                    {
+                        text: 'For justice',
+                        value: 'Batman'
+                    },
+                    {
+                        text: 'For popularity',
+                        value: 'Superman'
+                    }
+                ]
             },
             {
-                text: 'Fun and easy going',
-                value: 'The Flash'
-            }
-            ]
-        },
-        {
-            text: "Why did you want to become a superhero?",
-            responses: [{
-                text: 'For the thrills',
-                value: 'The Flash'
+                text: "Who would you most hang around with?",
+                responses: [{
+                        text: 'Wonder Woman',
+                        value: 'Superman'
+                    },
+                    {
+                        text: 'Green Arrow',
+                        value: 'The Flash'
+                    },
+                    {
+                        text: 'Robin',
+                        value: 'Batman'
+                    }
+                ]
             },
             {
-                text: 'For justice',
-                value: 'Batman'
+                text: "What's your favourite colour?",
+                responses: [{
+                        text: 'Black',
+                        value: 'Batman'
+                    },
+                    {
+                        text: 'Red',
+                        value: 'The Flash'
+                    },
+                    {
+                        text: 'Blue',
+                        value: 'Superman'
+                    }
+                ]
             },
             {
-                text: 'For popularity',
-                value: 'Superman'
-            }
-            ]
-        },
-        {
-            text: "Who would you most hang around with?",
-            responses: [{
-                text: 'Wonder Woman',
-                value: 'Superman'
+                text: "When do you help people?",
+                responses: [{
+                        text: 'Every chance I can',
+                        value: 'The Flash'
+                    },
+                    {
+                        text: 'At night',
+                        value: 'Batman'
+                    },
+                    {
+                        text: 'When they need me to',
+                        value: 'Superman'
+                    }
+                ]
             },
-            {
-                text: 'Green Arrow',
-                value: 'The Flash'
-            },
-            {
-                text: 'Robin',
-                value: 'Batman'
-            }
-            ]
-        },
-        {
-            text: "What's your favourite colour?",
-            responses: [{
-                text: 'Black',
-                value: 'Batman'
-            },
-            {
-                text: 'Red',
-                value: 'The Flash'
-            },
-            {
-                text: 'Blue',
-                value: 'Superman'
-            }
-            ]
-        },
-        {
-            text: "When do you help people?",
-            responses: [{
-                text: 'Every chance I can',
-                value: 'The Flash'
-            },
-            {
-                text: 'At night',
-                value: 'Batman'
-            },
-            {
-                text: 'When they need me to',
-                value: 'Superman'
-            }
-            ]
-        },
         ]
     };
 
@@ -98,15 +98,15 @@ window.onload = function () {
         },
         methods: {
             // Go to next question
-            next: function () {
+            next: function() {
                 this.questionIndex++;
                 console.log(this.userResponses);
             },
             // Go to previous question
-            prev: function () {
+            prev: function() {
                 this.questionIndex--;
             },
-            score: function () {
+            score: function() {
                 //find the highest occurence in responses
                 var modeMap = {};
                 var maxEl = this.userResponses[0],
