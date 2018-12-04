d38aefbb9a3b6a7cc3035a7ce4116922a1156dce
wolflint <wolflint@github.com>
2018-12-04 22:26:07 +0000

    More video support

--- index.php
+++ index.php
@@ -13,8 +13,10 @@
 <body>
   <div class="gradient"></div>
   <!-- BACKGROUND VIDEO -->
-  <video autoplay muted loop id="myVideo">
-    <source src="assets/videos/bgVideo.mp4" type="video/mp4" poster="assets/videos/bgVideo.png">
+  <video autoplay muted loop id="myVideo" poster="assets/videos/bgVideo.jpg">
+    <source src="assets/videos/bgVideo.mp4" type="video/mp4">
+    <source src="assets/videos/bgVideo.webm" type="video/webm">
+    <source src="assets/videos/bgVideo.ogv" type="video/ogg">
     Your browser does not support HTML5 video.
   </video>
 
