fc26de700a3b386d36d273aee92c77c06c187607
wolflint <wolflint@github.com>
2018-12-04 17:15:56 +0000

    Added info to chord guide

--- guidesChordDiagrams.php
+++ guidesChordDiagrams.php
@@ -18,6 +18,8 @@
     <div class="container">
         <h1>Groovy Guitar Guides - How to read the chord diagrams</h1>
 				<img src="assets/guidesChordDiagrams.jpg" alt="Picture of the guitar diagram labeled." class="u-full-width">
+        <p>The chord diagrams featured on this website represent the guitar fretboard from the first fret to the fifth fret. The vertical black lines represent the strings, with the one furthest to the left being the low E string (E2) and the right being the high E string (E4).</p>
+        <p>The grey dots represent the inlays on the guitar (most guitars have inlays on the 3rd, 5th, 9th and 12th frets). The circles indicate your fingers on your fretting hand, starting with the index finger (1) and ending with the pinky (4).</p>
 
     </div>
     <!-- SCRIPTS -->
